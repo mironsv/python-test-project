108adf6e3650f45dcd1d466c6aa7cc4cd0784615
Sergey Mironov <mironsv@mail.ru>
2022-09-11 17:03:42 +0300

    add calculate_mock test

--- tests/maintest.py
+++ tests/maintest.py
@@ -14,11 +14,22 @@ def args_based_return(*args, **kwargs):
         return Exception("exception occurred")
 
 
+def arg_calculate_return(*args, **kwargs):
+    return args[0] * 3
+
+
 class TestStringMethods(unittest.TestCase):
 
-    def test_mock(self):
+    def test_parametrized_mock(self):
         my_mock = ExchangeInterface('binance')
         my_mock.send_order = MagicMock(side_effect=args_based_return)
         assert send_order_with_amount(my_mock, 10) == "20 is sent to binance"
         assert send_order_with_amount(my_mock, 20) == "40 is sent to binance"
         assert send_order_with_amount(my_mock, 30) == "exception occurred is sent to binance"
+
+    def test_calculate_mock(self):
+        my_mock = ExchangeInterface('binance')
+        my_mock.send_order = MagicMock(side_effect=arg_calculate_return)
+        assert send_order_with_amount(my_mock, 10) == "30 is sent to binance"
+        assert send_order_with_amount(my_mock, 20) == "60 is sent to binance"
+        assert send_order_with_amount(my_mock, 30) == "90 is sent to binance"
